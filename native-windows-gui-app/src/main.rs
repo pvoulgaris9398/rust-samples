b28d4cd35a0e980cd3329cb9f306f823f58f0f07
Pete Voulgaris <petevoulgaris@gmail.com>
2025-06-28 20:52:24 -0400

    added rust native-windows-gui sample/research app

---
+++ native-windows-gui-app/src/main.rs
@@ -0,0 +1,53 @@
+// https://github.com/gabdube/native-windows-gui
+// https://docs.rs/native-windows-gui/latest/native_windows_gui/index.html
+#![windows_subsystem = "windows"]
+extern crate native_windows_derive as nwd;
+extern crate native_windows_gui as nwg;
+
+use nwd::NwgUi;
+use nwg::NativeUi;
+
+#[derive(Default, NwgUi)]
+pub struct BasicApp {
+    #[nwg_control(size: (300, 115), position: (300, 300), title: "Basic example", flags: "WINDOW|VISIBLE")]
+    #[nwg_events( OnWindowClose: [BasicApp::say_goodbye] )]
+    window: nwg::Window,
+
+    #[nwg_layout(parent: window, spacing: 1)]
+    grid: nwg::GridLayout,
+
+    #[nwg_control(text: "Josephine Momma", focus: true)]
+    #[nwg_layout_item(layout: grid, row: 0, col: 0)]
+    name_edit: nwg::TextInput,
+
+    #[nwg_control(text: "Say my name")]
+    #[nwg_layout_item(layout: grid, col: 0, row: 1, row_span: 2)]
+    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
+    hello_button: nwg::Button,
+}
+
+impl BasicApp {
+    fn say_hello(&self) {
+        nwg::modal_info_message(
+            &self.window,
+            "Hello",
+            &format!("Hello {}", self.name_edit.text()),
+        );
+    }
+
+    fn say_goodbye(&self) {
+        nwg::modal_info_message(
+            &self.window,
+            "Goodbye",
+            &format!("Goodbye {}", self.name_edit.text()),
+        );
+        nwg::stop_thread_dispatch();
+    }
+}
+
+fn main() {
+    nwg::init().expect("Failed to init Native Windows GUI");
+    nwg::Font::set_global_family("Segoe UI").expect("Failed to set default font");
+    let _app = BasicApp::build_ui(Default::default()).expect("Failed to build UI");
+    nwg::dispatch_thread_events();
+}
